515d8c347348cf5eb91e4d54139903987fcca163
hsiaofung <hsiaofung.chou@gmail.com>
2022-05-15 21:52:04 +0800

    first commit

---
+++ projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'lib-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.css']
+})
+export class CopyButtonComponent implements OnInit {
+
+  @Input() data = '';
+  @Output() copied = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onCopy() {
+  this.copied.next(0);
+  }
+}
---
+++ src/app/assassins.ts
@@ -0,0 +1,20 @@
+export const assassins = [
+    {
+        title: "A",
+        description: "A",
+        link: "www.yahoo.com.tw"
+        
+    },
+    {
+        title: "B",
+        description: "B",
+        link: "www.yahoo.com.tw"
+        
+    },
+    {
+        title: "C",
+        description: "C",
+        link: "www.yahoo.com.tw"
+        
+    }
+]
