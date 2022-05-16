7481f023be87b4d6e5771a3822e04c34474e7b9b
Tim Chou - TW (SW) <TimChou@supermicro.com>
2022-05-16 14:44:36 +0800

    add element

---
+++ projects/ui-elements/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { UiControlsModule, CopyButtonComponent } from 'ui-controls';
+// import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    // AppComponent
+  ],
+  imports: [BrowserModule, UiControlsModule],
+  providers: [],
+  bootstrap: [
+    // AppComponent
+  ],
+})
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(CopyButtonComponent, {
+      injector: this.injector,
+    });
+    customElements.define('copy-button', el);
+  }
+}
